26e9e456b7af256b09d8282f6c839636bf789687
Jørgen <jen@sevanssp.com>
2020-06-23 16:36:48 +0200

    Started SDKclient class

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 from modeltestSDK import SDKclient
-import requests
-import datetime
-from requests.exceptions import HTTPError
-
-'''
-resource = "campaign"
-version = ""
-endpoint = "all"
-print( "/" + "/".join([p for p in ["api", resource, version, endpoint] if p.strip()]))
 
 
 client = SDKclient()
-print(client.get('campaign', 'all', "/all/"))
+client.printAllCampaignNames()
+
+
 '''
-
-
-
-
 for url in ['http://127.0.0.1:8000/api/campaign/']:
     try:
         response = requests.post(url,
@@ -40,19 +29,4 @@ for url in ['http://127.0.0.1:8000/api/campaign/']:
         print('Posted!')
 print( response.json())
 
-for url in ['http://127.0.0.1:8000/api/campaign/all/']:
-    try:
-        response = requests.get(url)
-
-        # If the response was successful, no Exception will be raised
-        response.raise_for_status()
-    except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')  # Python 3.6
-    except Exception as err:
-        print(f'Other error occurred: {err}')  # Python 3.6
-    else:
-        print('Success!')
-
-camp_json= response.json()
-for camp in camp_json:
-    print(camp['name'])
+'''
--- modeltestSDK/client.py
+++ modeltestSDK/client.py
@@ -2,12 +2,33 @@ import os
 import sys
 import json
 import datetime
+import requests
+from requests.exceptions import HTTPError
 from .utils import to_snake_case, to_camel_case
+
 from .config import Config
-from .exceptions import AuthenticationError, ClientConnectionError, TimeSeriesAPIError
+
 
 class SDKclient:
 
     def __init__(self, config=Config):
         self.config =config
-        #self.time_series= TimeSeriesAPI(client=self)
+
+    def printAllCampaignNames(self):
+        urls = [self.config.host + "/" + self.config.base_url + "/campaign/all/"]
+        for url in urls:
+            try:
+                response = requests.get(url)
+
+                # If the response was successful, no Exception will be raised
+                response.raise_for_status()
+            except HTTPError as http_err:
+                print(f'HTTP error occurred: {http_err}')  # Python 3.6
+            except Exception as err:
+                print(f'Other error occurred: {err}')  # Python 3.6
+            else:
+                print('Success!')
+
+        camp_json = response.json()
+        for camp in camp_json:
+            print(camp['name'])
