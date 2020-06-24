f8501a81f4d45532a9b3ce14ab168b4e5e24ca3a
Jørgen <jen@sevanssp.com>
2020-06-24 10:16:04 +0200

    Added post and get requests

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from modeltestSDK import SDKclient
+import datetime
 
 
 client = SDKclient()
-client.printAllCampaignNames()
 
-
-'''
-for url in ['http://127.0.0.1:8000/api/campaign/']:
-    try:
-        response = requests.post(url,
-                                 json={'name': 'SDKtesting2',
+client.post("campaign",body={'name': 'SDKt2',
                                        'description': 'A campaign added from SDK2',
                                        'location': 'string',
                                        'date': (datetime.datetime.utcnow()).isoformat(),
@@ -18,15 +13,8 @@ for url in ['http://127.0.0.1:8000/api/campaign/']:
                                        'water_density': 0,
                                        'water_depth': 0,
                                        'transient': 0})
+campaign = client.get("campaign","all", parameters={"name":"SDKt"})
 
-        # If the response was successful, no Exception will be raised
-        response.raise_for_status()
-    except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')  # Python 3.6
-    except Exception as err:
-        print(f'Other error occurred: {err}')  # Python 3.6
-    else:
-        print('Posted!')
-print( response.json())
+for camp in campaign:
+    print(camp['name'])
 
-'''
--- modeltestSDK/client.py
+++ modeltestSDK/client.py
@@ -2,6 +2,8 @@ import os
 import sys
 import json
 import datetime
+import urllib.parse
+
 import requests
 from requests.exceptions import HTTPError
 from .utils import to_snake_case, to_camel_case
@@ -12,23 +14,36 @@ from .config import Config
 class SDKclient:
 
     def __init__(self, config=Config):
-        self.config =config
+        self.config = config
 
-    def printAllCampaignNames(self):
-        urls = [self.config.host + "/" + self.config.base_url + "/campaign/all/"]
-        for url in urls:
-            try:
-                response = requests.get(url)
+    def do_request(self, method, resource: str, endpoint: str = "", parameters: dict = None, body: dict = None):
+        url = self.config.host +"/" + "/".join([p for p in [self.config.base_url, resource, endpoint] if p.strip()])
 
-                # If the response was successful, no Exception will be raised
-                response.raise_for_status()
-            except HTTPError as http_err:
-                print(f'HTTP error occurred: {http_err}')  # Python 3.6
-            except Exception as err:
-                print(f'Other error occurred: {err}')  # Python 3.6
-            else:
-                print('Success!')
+        if parameters is not None and isinstance(parameters, dict):
+            parameters = to_camel_case({k: v for k, v in parameters.items() if v is not None})
+            enc_parameters = urllib.parse.urlencode(parameters, quote_via=urllib.parse.quote)
+        else:
+            enc_parameters = ""
 
-        camp_json = response.json()
-        for camp in camp_json:
-            print(camp['name'])
+        query_url= f"{url}/?{enc_parameters}"
+
+        try:
+            response = method(query_url,json=body)
+            response.raise_for_status()
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')
+            return err
+        except Exception as err:
+            print(f'Other error occurred: {err}')
+            return err
+        else:
+            return response.json()
+
+    def get(self,resource: str, endpoint: str = "", parameters: dict = None):
+        return self.do_request(requests.get, resource, endpoint, parameters=parameters)
+
+    def post(self,resource: str, endpoint: str = "", body: dict = None):
+        return self.do_request(requests.post, resource, endpoint, body=body)
+
+    #def delete(self, resource: str, endpoint: str, parameters: dict = None):
+    #    return self.do_request(requests.delete,resource,)
