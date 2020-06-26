30d9eb1944924a053ba4e1a566607d5e62b2d930
Jørgen <jen@sevanssp.com>
2020-06-26 10:04:03 +0200

    Test testing

--- modeltestSDK/tests/test_campaign.py
+++ modeltestSDK/tests/test_campaign.py
@@ -4,6 +4,6 @@ from modeltestSDK.resources import Campaign
 
 def test_types(new_campaign):
     assert isinstance(new_campaign, Campaign)
-    #assert isinstance(new_campaign.id, str)
+    assert isinstance(new_campaign.id, str)
     assert isinstance(new_campaign.name, str)
     assert isinstance(new_campaign.description, str)
