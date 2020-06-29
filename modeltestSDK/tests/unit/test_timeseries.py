e53c321ca91f46bc8f5b34e819d230e1e01c6a23
Jørgen <jen@sevanssp.com>
2020-06-29 09:35:07 +0200

    Added more tests

--- modeltestSDK/tests/unit/test_timeseries.py
+++ modeltestSDK/tests/unit/test_timeseries.py
@@ -4,6 +4,6 @@ from sqlalchemy.dialects.postgresql import UUID
 
 def test_types(new_timeseries):
     #assert isinstance(new_timeseries.id, (uuid.UUID, UUID))
-    assert isinstance(new_timeseries.sensor_id, (uuid.UUID, UUID))
-    assert isinstance(new_timeseries.test_id, (uuid.UUID, UUID))
+    assert isinstance(new_timeseries.sensor_id, str)
+    assert isinstance(new_timeseries.test_id, str)
 
