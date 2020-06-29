cea94eaa0dcbb839e71f44d6505b011bdc305031
Jørgen <jen@sevanssp.com>
2020-06-29 14:02:33 +0200

    Added pipeline folder, started add_demo_data

--- pipeline/create_demo_data.py
+++ pipeline/create_demo_data.py
@@ -1,19 +1,21 @@
-from .. modeltestSDK import Campaign, SDKclient
+from modeltestSDK.resources import Campaign, Test
+from modeltestSDK.client import SDKclient
+import datetime
 
 
 data = [
-        dict(cname="Electron GmbH2",
-             cdescr="Development of floating electrical substation for harsh environment.",
-             date=datetime.utcnow(),
+        dict(name="Electron GmbH2",
+             description="Development of floating electrical substation for harsh environment.",
+             date=datetime.datetime.utcnow(),
              location="Stadt",
              diameter=60,
              scale_factor=1,
              water_density=1025,
              water_depth=900,
              transient=200.),
-        dict(cname="Aladdin Oil2",
-             cdescr="Development of FPSO for arctic environment.",
-             date=datetime.utcnow(),
+        dict(name="Aladdin Oil2",
+             description="Development of FPSO for arctic environment.",
+             date=datetime.datetime.utcnow(),
              location="Sintef",
              diameter=40,
              scale_factor=1,
@@ -22,11 +24,33 @@ data = [
              transient=200.),
     ]
 
-def main():
-    for item in data:
-        Campaign.from_dict(item, client)
+
+def demo_campaign(client):
+    client.campaign.create(name="SWATCH",
+             description="Development of floating electrical substation for harsh environment.",
+             date=(datetime.datetime.utcnow()).isoformat(),
+             location="Stadt",
+             diameter=60,
+             scale_factor=1,
+             water_density=1025,
+             water_depth=900,
+             transient=200.2)
+    client.campaign.create(name="Aladdin Oil2",
+             description="Development of FPSO for arctic environment.",
+             date=(datetime.datetime.utcnow()).isoformat(),
+             location="Sintef",
+             diameter=40,
+             scale_factor=1,
+             water_density=1025,
+             water_depth=950,
+             transient=200.)
+
+
 
 
 if __name__ == "__main__":
     client = SDKclient()
-    main()
+    demo_campaign(client)
+
+    camp_id = client.campaign.get_id("SWATCH")
+    client.test.create(dict(description = "wavecalibration", test_date = (datetime.datetime.utcnow()).isoformat(), direction = "north", campaign_id = camp_id, type = "Floater"))
