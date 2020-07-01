c76aba3ce7bfca04ff5a70fa77e2808303dca449
Jørgen <jen@sevanssp.com>
2020-07-01 14:33:32 +0200

    Pipeline: bug fixing

--- pipeline/add_timeseries.py
+++ pipeline/add_timeseries.py
@@ -2,15 +2,15 @@ from modeltestSDK.resources import Campaign, Sensor, DataPoint, Timeseries
 from modeltestSDK.client import SDKclient
 import datetime
 import pandas as pd
-import time as TIME
+import time as timer
 
 def read_datapoints_from_csv_with_pandas(file, test_id,client: SDKclient):
-    df = pd.read_csv(file, sep=';').head(1000)
+    df = pd.read_csv(file, sep=';').head(10000)
 
     col_names = list(df.columns)
     for sensor in col_names[1:]:
         sensor_strip = sensor.strip()
-        tic =TIME.perf_counter()
+        tic =timer.perf_counter()
         sensor_id = client.sensor.get_id(sensor_strip)
         timeseries = client.timeseries.create(sensor_id=sensor_id,
                                 test_id=test_id)
@@ -18,10 +18,10 @@ def read_datapoints_from_csv_with_pandas(file, test_id,client: SDKclient):
         #print(col_names[0], sensor)
         for time, value in datapoints:
             datapoint = DataPoint(timeseries_id=timeseries.id,
-                                  time=datetime.datetime.utcnow().isoformat(),
+                                  time=datetime.datetime.strptime(time, "%H:%M %S.%f").isoformat(),
                                   value=value,
                                   client=client)
             timeseries.data_points.append(datapoint)
         timeseries.post_data_points()
-        toc = TIME.perf_counter()
+        toc = timer.perf_counter()
         print(f"Posting timeseries for sensor {sensor} in file {file} took  {toc - tic:0.4f} seconds")
