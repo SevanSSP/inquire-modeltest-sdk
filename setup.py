79fad5babb6f60140dd14f8717200a860c7b28e1
joreng2607 <66948196+joreng2607@users.noreply.github.com>
2020-07-07 11:25:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="modeltestSDK",
-    version="0.0.1",
+    version="0.0.2",
     author="Jørgen",
     author_email="jen@sevanssp.com",
     description="A small example package",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
