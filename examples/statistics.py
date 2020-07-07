f3b7b378241b3c654e75d2918ce2525b7437beb1
nicolai-sevan <nbu@sevanssp.com>
2020-07-07 16:59:18 +0200

    commented out some code and began on improved code that does the same thing

--- examples/statistics.py
+++ examples/statistics.py
@@ -24,8 +24,8 @@ mplstyle.use('fast')
 
 client = SDKclient()
 
+'''
 tests = client.floater.get_all()
-
 
 for test in tests:
     scale_factor = 75
@@ -94,14 +94,14 @@ for test in tests:
 
                 #timeseries = TimeSeries('Full scale Navn', t, v)
                 # Plotting
-                '''plt.figure(1, figsize=(20, 6), facecolor='w', edgecolor='k')
-                plt.title('Timeseries')
-                plt.plot(t, X)
-                plt.xlabel('Time [s]')
-                plt.ylabel("Amplitude (mm)")
-                plt.legend(['Free Decay Test sample'], loc='upper left')
-                plt.grid()
-                plt.show()'''
+                #plt.figure(1, figsize=(20, 6), facecolor='w', edgecolor='k')
+                #plt.title('Timeseries')
+                #plt.plot(t, X)
+                #plt.xlabel('Time [s]')
+                #plt.ylabel("Amplitude (mm)")
+                #plt.legend(['Free Decay Test sample'], loc='upper left')
+                #plt.grid()
+                #plt.show()
 
                 print("Test er", test.description, "og sensor er", sensor.name)
 
@@ -134,49 +134,29 @@ for test in tests:
         print(''.join(column.rjust(15) for column in header), file=open(print_path, "a"))
         for row in table:
             print(''.join(f'{column:.3f}'.rjust(15) for column in row), file=open(print_path, "a"))
-
-
-
-# gammelt
-'''
-            fullSurge.t, fullSurge.x = fullSurge.filter('hp', 1. / 30., twin=(100, 1e8), taperfrac=None)
-            fullPitch.t, fullPitch.x = fullPitch.filter('hp', 1. / 27., twin=(100, 1e8), taperfrac=None)
-            fullAcc.t, fullAcc.x = fullAcc.filter('hp', 1. / 27., twin=(100, 1e8), taperfrac=None)
-            fullZacc.t, fullZacc.x = fullZacc.filter('hp', 1. / 30., twin=(100, 1e8), taperfrac=None)
-            # f_sigx, tf_acc = tfe(fullWave.x, fullAcc.x, dt, clim=coherence, nperseg=8700)
-
-            print_path = os.path.join(os.path.split(os.getcwd())[0], 'Results', nameofresults)
-            a = fullSurge.stats(statsdur=10800.)
-            b = fullPitch.stats(statsdur=10800.)
-            c = fullAcc.stats(statsdur=10800.)
-
-            d = fullHeave.stats(statsdur=10800.)
-            e = fullZacc.stats(statsdur=10800.)
-
-            header = ['Motion', 'std', 'min', 'max']
-            table = [[1, a['std'], a['min'], a['max']], [3, d['std'], d['min'], d['max']],
-                     [5, b['std'], b['min'], b['max']], [11, c['std'], c['min'], c['max']],
-                     [33, e['std'], e['min'], e['max']]]
-            print("--------------------------------------------------------------", file=open(print_path, "a"))
-            print('namevec[k]' + ' Surge = 1, Heave = 3, Pitch = 5, Horizontal Acc = 11, vert acc = 33',
-                  file=open(print_path, "a"))
-            print("---------------------------------------------------------------", file=open(print_path, "a"))
-            print(''.join(column.rjust(15) for column in header), file=open(print_path, "a"))
-            for row in table:
-                print(''.join(f'{column:.3f}'.rjust(15) for column in row), file=open(print_path, "a"))
 '''
 
 
+tests = ["waveIrreg_2101", "waveIrreg_2102", "waveIrreg_2103", "waveIrreg_2104", "waveIrreg_2105"]
 
-    #else:
-        #print("ikke irreg")
+stt = client.campaign.get_by_name("STT")
 
-#timeseries = client.timeseries.get("000998a6-dac6-4c51-ab46-9359518e5878")
-#stddev = timeseries.standard_deviation()
-#print(stddev)
+for test_name in tests:
+    test = client.floater.get_by_name(test_name)
+    stt.populate_test(test)
+    timeseries = test.get_timeseries()
+    stt.test[test_name].populate_timeseries(timeseries)
+
+    sensor_names = ["M206_COF X", "M206_COF Z", "M206_COF Pitch", "M206_acc_pos X", "M206_acc_pos Z"]
+    for sensor_name in sensor_names:
+        ts = stt.test[test_name].timeseries[sensor_name]
+        # boom, her gjør man resten for alle tidsseriene
+
+
+
+
 
 '''
-
 # finn spesifikk tidsserie på enkel måte:
 campaigns = client.campaign.get_all()
 
@@ -184,11 +164,13 @@ print(campaigns)
 
 #campaign_id = client.campaign.get_id("STT")
 stt = client.campaign.get_by_name("STT")
+print(stt)
 #print(stt.get_tests())
 
 #test_id = client.floater.get_id("waveIrreg_2101")
 test = client.floater.get_by_name("waveIrreg_2101")
 stt.populate_test(test)
+print(test)
 
 timeseries = test.get_timeseries()
 stt.test["waveIrreg_2101"].populate_timeseries(timeseries)
@@ -196,32 +178,36 @@ stt.test["waveIrreg_2101"].populate_timeseries(timeseries)
 print(stt.test["waveIrreg_2101"].timeseries["M206_COG X"])
 
 ts = stt.test["waveIrreg_2101"].timeseries["M206_COG X"]
+print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+print(ts.sensor_id)
 datapoints = ts.get_data_points()
 times = []
-                v = []
-                # Fyller inn tid og verdi fra datapunkter til en tidsvektor og verdivektor
-                for datapoint in datapoints:
-                    times.append(datapoint.time)
-                    v.append(datapoint.value)
-                # Må konvertere t-vektor til antall sekunder fra start-tiden
-                times_in_array = numpy.array(times)
-                start_time = times_in_array[0]
-                t = []
-                for ti in times_in_array:
-                    t.append((ti-start_time).total_seconds())
-                t = numpy.array(t)
+v = []
 
-                # Plotting
-                plt.figure(1, figsize=(20, 6), facecolor='w', edgecolor='k')
-                plt.title('Timeseries')
-                plt.plot(t, v)
-                plt.xlabel('Time [s]')
-                plt.ylabel("Amplitude (mm)")
-                plt.legend(['Free Decay Test sample'], loc='upper left')
-                plt.grid()
-                plt.show()
+# Fyller inn tid og verdi fra datapunkter til en tidsvektor og verdivektor
+for datapoint in datapoints:
+    times.append(datapoint.time)
+    v.append(datapoint.value)
 
-                print("Test er", test.description, "og sensor er", sensor.name)'''
+# Må konvertere t-vektor til antall sekunder fra start-tiden
+times_in_array = numpy.array(times)
+start_time = times_in_array[0]
+t = []
+for ti in times_in_array:
+    t.append((ti-start_time).total_seconds())
+t = numpy.array(t)
+
+# Plotting
+#plt.figure(1, figsize=(20, 6), facecolor='w', edgecolor='k')
+#plt.title('Timeseries')
+#plt.plot(t, v)
+#plt.xlabel('Time [s]')
+#plt.ylabel("Amplitude (mm)")
+#plt.legend(['Free Decay Test sample'], loc='upper left')
+#plt.grid()
+#plt.show()
+
+#print("Test er", test.description, "og sensor er", sensor.name)'''
 
 
 
