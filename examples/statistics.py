17a108a6e947b1b5d4b384e39ea1815eac04c66c
nicolai-sevan <nbu@sevanssp.com>
2020-07-08 09:29:28 +0200

    some changes to the statistics script to use more SDK features

--- examples/statistics.py
+++ examples/statistics.py
@@ -137,9 +137,11 @@ for test in tests:
 '''
 
 
-tests = ["waveIrreg_2101", "waveIrreg_2102", "waveIrreg_2103", "waveIrreg_2104", "waveIrreg_2105"]
-
 stt = client.campaign.get_by_name("STT")
+tests = ["waveIrreg_2101", "waveIrreg_2102", "waveIrreg_2103", "waveIrreg_2104", "waveIrreg_2105"]
+sensor_names = ["M206_COF X", "M206_COF Z", "M206_COF Pitch", "M206_acc_pos X", "M206_acc_pos Z"]
+coherence = 0.7
+results_file = "statistics_SWACH.txt"
 
 for test_name in tests:
     test = client.floater.get_by_name(test_name)
@@ -147,10 +149,101 @@ for test_name in tests:
     timeseries = test.get_timeseries()
     stt.test[test_name].populate_timeseries(timeseries)
 
-    sensor_names = ["M206_COF X", "M206_COF Z", "M206_COF Pitch", "M206_acc_pos X", "M206_acc_pos Z"]
+
     for sensor_name in sensor_names:
         ts = stt.test[test_name].timeseries[sensor_name]
-        # boom, her gjør man resten for alle tidsseriene
+        print("Hentet datapoints for", sensor_name)
+        times = []
+        XX = []
+        ts.data_points
+        # Fyller inn tid og verdi fra datapunkter til en tidsvektor og verdivektor
+        for datapoint in ts.data_points:
+            times.append(datapoint.time)
+            XX.append(datapoint.value)
+
+        # Må konvertere t-vektor til antall sekunder fra testen startet
+        times_in_array = numpy.array(times)
+        start_time = times_in_array[0]
+        tt = []
+        for Time in times_in_array:
+            tt.append((Time - start_time).total_seconds())
+        tt = numpy.array(tt)
+        XX = numpy.array(XX)
+
+        # Froude skalering
+        if sensor_name == "M206_COF X":
+            power = 1
+        if sensor_name == "M206_COF Z":
+            power = 1
+        if sensor_name == "M206_COF Pitch":
+            power = 0
+        if sensor_name == "M206_acc_pos X":
+            power = 0
+        if sensor_name == "M206_acc_pos Z":
+            power = 0
+
+
+        t = tt * (stt.scale_factor ** 0.5)
+        if power == 0:
+            X = XX
+        else:
+            X = XX * (stt.scale_factor ** power) / 1000
+
+        # TimeSeries fra qatz
+        w = TimeSeries('Full Scale' + sensor_name, t, X)
+        if sensor_name == "M206_COF X":
+            fullSurge = w
+        if sensor_name == "M206_COF Z":
+            fullHeave = w
+        if sensor_name == "M206_COF Pitch":
+            fullPitch = w
+        if sensor_name == "M206_acc_pos X":
+            fullAcc = w
+        if sensor_name == "M206_acc_pos Z":
+            fullZacc = w
+
+        # Plotting
+        # plt.figure(1, figsize=(20, 6), facecolor='w', edgecolor='k')
+        # plt.title('Timeseries')
+        # plt.plot(t, X)
+        # plt.xlabel('Time [s]')
+        # plt.ylabel(ts.get_sensor().kind)
+        # plt.legend(['Free Decay Test sample'], loc='upper left')
+        # plt.grid()
+        # plt.show()
+
+        print("Test er", test.description, "og sensor er", sensor_name)
+
+    newSurge_t, newSurge_x = fullSurge.filter('hp', 1. / 30., twin=(100, 1e8), taperfrac=None)
+    fullPitch_t, fullPitch_x = fullPitch.filter('hp', 1. / 27., twin=(100, 1e8), taperfrac=None)
+    # fullAcc_t, fullAcc_x = fullAcc.filter('hp', 1. / 27., twin=(100, 1e8), taperfrac=None)
+    # fullZacc.t, fullZacc.x = fullZacc.filter('hp', 1. / 30., twin=(100, 1e8), taperfrac=None)
+    # f_sigx, tf_acc = tfe(fullWave.x, fullAcc.x, dt, clim=coherence, nperseg=8700)
+
+    # wavestats = fullWave.stats(statsdur=10800)
+
+    newSurge = TimeSeries("New Surge", newSurge_t, newSurge_x)
+    newPitch = TimeSeries("New Pitch", fullPitch_t, fullPitch_x)
+
+    print_path = os.path.join(os.path.split(os.getcwd())[0], results_file)
+    print(print_path)
+    a = newSurge.stats(statsdur=10800.)
+    b = newPitch.stats(statsdur=10800.)
+    # c = fullAcc.stats(statsdur=10800.)
+
+    d = fullHeave.stats(statsdur=10800.)
+    # e = fullZacc.stats(statsdur=10800.)
+
+    header = ['Motion', 'std', 'min', 'max']
+    table = [[1, a['std'], a['min'], a['max']], [3, d['std'], d['min'], d['max']],
+             [5, b['std'], b['min'], b['max']]]
+    print("--------------------------------------------------------------", file=open(print_path, "a"))
+    print(test_name, ' Surge = 1, Heave = 3, Pitch = 5', file=open(print_path, "a"))
+    print("---------------------------------------------------------------", file=open(print_path, "a"))
+    print(''.join(column.rjust(15) for column in header), file=open(print_path, "a"))
+    for row in table:
+        print(''.join(f'{column:.3f}'.rjust(15) for column in row), file=open(print_path, "a"))
+
 
 
 
