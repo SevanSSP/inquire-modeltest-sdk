dbed572df8571e90bb94b32468884d0d10dc8519
Jørgen <jen@sevanssp.com>
2020-07-09 14:48:36 +0200

    Start setup for testing

---
+++ modeltestSDK/tests/client/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from modeltestSDK import SDKclient
+
+@pytest.fixture(scope="module")
+def client():
+    '''Expose an SDK API client configured for testing'''
+    client = SDKclient() #TODO: Konfigurer en test API (lokalt?)
+    return client
+
