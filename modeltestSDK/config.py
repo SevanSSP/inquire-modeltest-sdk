4f02ac2d824abe44742a1b10ed465761f241d821
Jørgen <jen@sevanssp.com>
2020-07-09 12:07:30 +0200

    Fix

--- modeltestSDK/config.py
+++ modeltestSDK/config.py
@@ -1,5 +1,4 @@
 class Config:
-    def __init__(self):
-        # host = "https://inquire-modeltest-docker.azurewebsites.net"
-        self.host = "http://127.0.0.1:8000"
-        self.base_url = "api"
+
+    host = "http://127.0.0.1:8000"
+    base_url = "api"
