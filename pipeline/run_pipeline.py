c32b18fe7bcb62045ab6b51e4cf28e5705d2dc6c
nicolai-sevan <nbu@sevanssp.com>
2020-08-12 14:25:43 +0200

    comments that explain add_sensors, also more comments on run_pipeline script

--- pipeline/run_pipeline.py
+++ pipeline/run_pipeline.py
@@ -18,16 +18,22 @@ def main():
     tic = time.perf_counter()
 
     client = SDKclient()
-    campaign_dir = "C:/Users/hly/Documents/STT"
-    campaign = client.campaign.create(name=campaign_dir.split("/")[-1],
-                                      description="Modeltest for SWACH",
+
+    # Specify path to folder where campaign is locally stored
+    campaign_dir = "C:/Users/nbu/Documents/STT"
+
+    # Create initial campaign in database
+    campaign = client.campaign.create(name="STT",
+                                      description="Modeltest for SWACH and HE Modu",
                                       date=get_datetime_date("180120120000"),
                                       location="STADT TOWING TANK",
-                                      waterline_diameter=70,  # main hull cylinder
-                                      scale_factor=75,  # står i rapporten
-                                      water_density=1025,  # usikkert
-                                      water_depth=4.1 * 75,  # Kilde på at dybden i tanken er 4.1m er fisk.no, fant ikke noe annet offisielt tall
-                                      transient=3 * 60 * 60)  # 3 hours in seconds)
+                                      waterline_diameter=70,    # Main hull cylinder
+                                      scale_factor=75,          # From the report
+                                      water_density=1025,       # Not sure if this is correct
+                                      water_depth=4.1 * 75,     # Source for water depth being 4.1m is fisk.no, could not find any other official number
+                                      transient=3 * 60 * 60)    # 3 hours in seconds
+
+    # Add all the sensors that were used in STT campaign
     add_sensors(campaign=campaign, client=client)
 
     concept_ids = ["M206", "M207"]
