09818588cff7e4d9800b091a224cfa9138cb218c
joreng2607 <66948196+joreng2607@users.noreply.github.com>
2021-01-18 15:04:08 +0100

    Update config.py

--- modeltestSDK/config.py
+++ modeltestSDK/config.py
@@ -8,6 +8,7 @@ class Config:
     Select between local host and azure
     '''
     #host = "https://inquire-modeltest-docker.azurewebsites.net"
-    host = "http://127.0.0.1:8000"
+    host = "https://inquire-modeltest-eqn.azurewebsites.net/"
+    #host = "http://127.0.0.1:8000"
     base_url = "api"
     version = 'v1'
