35406908eaa95fa4ae17868c4aaf1ff9da81c0e5
jen <jen@sevanssp.com>
2021-01-19 14:55:27 +0100

    Merge conflicts

--- modeltestSDK/config.py
+++ modeltestSDK/config.py
@@ -7,8 +7,8 @@ class Config:
     """
     Select between local host and azure
     """
-    #host = "https://inquire-modeltest-docker.azurewebsites.net"
+    # host = "https://inquire-modeltest-docker.azurewebsites.net"
     host = "https://inquire-modeltest-eqn.azurewebsites.net/"
-    #host = "http://127.0.0.1:8000"
+    # host = "http://127.0.0.1:8000"
     base_url = "api"
     version = 'v1'
