33dde9646fb489366c340cc1eba2f1e7f1c7cf4f
jen <jen@sevanssp.com>
2021-01-20 14:45:40 +0100

    Pipeline continued

--- pipeline/Luva/add_test.py
+++ pipeline/Luva/add_test.py
@@ -3,7 +3,7 @@ import re
 import datetime
 from scipy.io import loadmat
 from modeltestSDK import SDKclient, Campaign
-from .add_timeseries import read_datapoints_from_mat_with_pandas, read_wave_calibration_from_mat_with_pandas
+from .add_timeseries import read_datapoints
 
 category_dict = {'Current': 'current force', 'Wind': 'wind force', 'Decay': 'decay', 'IrrWave': 'irregular wave',
                  'RegWave': 'regular wave', }
@@ -13,7 +13,7 @@ def add_tests(campaign_dir, campaign: Campaign, client: SDKclient):
     existing_wave_calibrations = {}
 
     os.chdir(campaign_dir)
-    os.chdir(os.getcwd() + "\\" + "Analysis/Timeseries")
+    os.chdir(os.getcwd() + "\\" + "Analysis/Timeseries (To be modified)")
     test_categories = os.listdir(path='.')
     for category in test_categories:
 
@@ -25,20 +25,49 @@ def add_tests(campaign_dir, campaign: Campaign, client: SDKclient):
             category = category_dict[category]
 
         for test in tests:
+            if not test.endswith('.mat'):
+                continue
             if os.path.isdir(test):
                 continue
             data = loadmat(os.getcwd() + "\\" + test)
 
             test_date_str = str(data['test_date'])[2:-2]
-            test_date = datetime.datetime.strptime(test_date_str, '%H:%M %d/%m/%y').isoformat()
+            test_date = datetime.datetime.strptime(test_date_str, '%Y-%m-%d %H:%M').isoformat()
 
             test_description = str(data['comment'])[2:-2]
-            print(test_description)
 
-            number = data['test_num']
+            test_number = str(data['test_num'][0])[1:-1]
 
             wave_calibration_id = None
             wave_cal = ["WAVE_1_CAL", "WAVE_2_CAL", "WAVE_3_CAL"]
+
+            if category == "current force":
+                os.chdir(os.getcwd() + "\\Calib")
+                calibration = os.listdir(path='.')[0]
+                calibration_data = loadmat(os.getcwd() + "\\" + calibration)
+                calibration_description = str(calibration_data['comment'])[2:-2]
+                calibration_number = str(calibration_data['test_num'][0])[1:-1]
+                calibration_date = str(calibration_data['test_date'])[2:-2]
+                current_vel_str = re.findall("\d+,\d+", calibration_description)[0]
+                current_vel = float(current_vel_str.replace(',', '.'))
+
+                current_calibration = client.wave_calibration.create(number=calibration_number,
+                                                                     description=calibration_description,
+                                                                     test_date=calibration_date,
+                                                                     campaign_id=campaign.id,
+                                                                     wave_spectrum="jonswap",
+                                                                     wave_height=0,
+                                                                     wave_period=0,
+                                                                     gamma=0,
+                                                                     wave_direction=0,
+                                                                     current_velocity=current_vel,
+                                                                     current_direction=0,  # TODO: Direction?
+                                                                     read_only=True, )
+                read_datapoints(data=calibration_data, test=current_calibration, client=client)
+
+                wave_calibration_id = current_calibration.id
+
+                os.chdir('..')
 
             if category == "irregular wave":
                 sea_state = re.findall("\d+\.\d+", test_description)
@@ -57,7 +86,7 @@ def add_tests(campaign_dir, campaign: Campaign, client: SDKclient):
                     cal_test_num = "unknown wave calib"
 
                 if cal_test_num not in existing_wave_calibrations:
-                    wave_calibration = client.wave_calibration.create(number=str(cal_test_num),
+                    wave_calibration = client.wave_calibration.create(number=cal_test_num,
                                                                       description=test_description,
                                                                       test_date=test_date,
                                                                       campaign_id=campaign.id,
@@ -87,7 +116,7 @@ def add_tests(campaign_dir, campaign: Campaign, client: SDKclient):
                     config_index = floater_configs[floater_configs['name'] == config_name].index.values
                     floaterconfig_id = floater_configs.loc[config_index, 'id'].tolist()[0]
 
-            test = client.floater_test.create(number=str(number),
+            test = client.floater_test.create(number=test_number,
                                               description=test_description,
                                               test_date=test_date,
                                               campaign_id=campaign.id,
@@ -99,15 +128,6 @@ def add_tests(campaign_dir, campaign: Campaign, client: SDKclient):
 
             # Todo: Add floater_test tags
 
-            read_datapoints_from_mat_with_pandas(data=data, test=test, skip_channels=wave_cal, client=client,
-                                                 derive_channels={
-                                                     "ZPOS_WL_Sevan": {'from': "ZPOS_WL", 'factors': [-1, 0]},
-                                                     "YPOS_WL_Sevan": {'from': "YPOS_WL", 'factors': [-1, 0]},
-                                                     "YAW_Sevan": {'from': "YAW", 'factors': [-1, 0]},
-                                                     "PITCH_Sevan": {'from': "PITCH", 'factors': [-1, 0]},
-                                                     "ZPOS_TOPDKC_Sevan": {'from': "ZPOS_TOPDKC", 'factors': [-1, 0]},
-                                                     "YPOS_TOPDKC_Sevan": {'from': "ZPOS_TOPDKC", 'factors': [-1, 0]},
-                                                     "RELW_01_Sevan": {'from': "RELW_01", 'factors': [1, 0]},
-                                                     "WAVE_3_Sevan": {'from': "WAVE_3", 'factors': [1, 0]}})
+            read_datapoints(data=data, test=test, skip_channels=wave_cal, client=client)
 
-        os.chdir(campaign_dir + "\\" + "Analysis/Timeseries")
+        os.chdir(campaign_dir + "\\" + "Analysis/Timeseries (To be modified)")
