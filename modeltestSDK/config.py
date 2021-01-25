de23c7e3b7c23a96d3ebb5ef5a8f1e63f1e8841a
tovop <per.voie@gmail.com>
2021-01-25 15:21:51 +0100

    Generalized the client configuration and specified the default host.

--- modeltestSDK/config.py
+++ modeltestSDK/config.py
@@ -1,14 +1,13 @@
 """
 Configurations
 """
+import os
 
 
 class Config:
     """
     Select between local host and azure
     """
-    # host = "https://inquire-modeltest-docker.azurewebsites.net"
-    # host = "https://inquire-modeltest-eqn.azurewebsites.net/"
-    host = "http://127.0.0.1:8000"
+    host = os.environ.get("INQUIRE_MODELTEST_API_HOST", "https://inquire-modeltest.azurewebsites.net")
     base_url = "api"
     version = 'v1'
