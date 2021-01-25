ecf864761a7a36925a72af3a57d0af9d928d68d5
tovop <per.voie@gmail.com>
2021-01-25 15:22:15 +0100

    Cleaned the imports.

--- modeltestSDK/__init__.py
+++ modeltestSDK/__init__.py
@@ -1,8 +1,4 @@
 """
 Python SDK for inquire modeltest API
 """
-
-from .api import CampaignAPI, SensorAPI, TimeseriesAPI, TestAPI, FloaterConfigAPI, FloaterTestAPI
-from .resources import Campaign, Sensor, Test, BaseResource, Timeseries, DataPoint
-from .client import SDKclient
-from .plot_timeseries import plot_timeseries
+from .client import Client
