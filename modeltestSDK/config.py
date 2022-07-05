72dfbfc1ac143d91ec0224b4de41c7435a3e6b0e
Snorre Ludvig Fjellvang <slf@sevanssp.com>
2022-07-05 13:30:05 +0200

    "

--- modeltestSDK/config.py
+++ modeltestSDK/config.py
@@ -8,7 +8,7 @@ class Config:
     """
     Client configuration
     """
-    host: str = os.environ.get("INQUIRE_MODELTEST_API_HOST")
+    host: str = os.environ.get("https://inquire-modeltest-eqn.azurewebsites.net")
     base_url: str = "api"
     version: str = "v1"
     log_level: str = "info"
@@ -22,5 +22,5 @@ class LocalConfig(Config):
     """
     Local context configuration
     """
-    host: str = os.environ.get("INQUIRE_MODELTEST_API_HOST", "http://localhost:8000/")
+    host: str = os.environ.get("INQUIRE_MODELTEST_API_HOST", "http://localhost:5000/")
 
--- tests/functional/test_user.py
+++ tests/functional/test_user.py
@@ -2,12 +2,12 @@ import requests
 
 
 def test_create_user(client) -> None:
-    user_dict = dict(username='tester44234',
+    user_dict = dict(username='tester494234',
                      full_name='',
                      email='test@testing.com',
                      password='password',
                      disabled=False)
 
-    resp = requests.post('http://localhost:8000/api/v1/auth/users?administrator_key=administrator', json=user_dict)
+    resp = requests.post('http://localhost:8080/api/v1/auth/users?administrator_key=administrator', json=user_dict)
     assert resp.status_code == 200
 
