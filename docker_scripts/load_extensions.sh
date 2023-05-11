b2aacaef91dac70da7c5ecd27ed832b1d14423cf
Snorre Ludvig Fjellvang <slf@sevanssp.com>
2023-05-11 14:23:51 +0200

    Cleanup

--- docker_scripts/load_extensions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# You could probably do this fancier and have an array of extensions
-# to create, but this is mostly an illustration of what can be done
-
-psql -v ON_ERROR_STOP=1 --username postgres --dbname test_modeltest_db <<EOF
-create extension "UUID-OSSP";
-EOF
