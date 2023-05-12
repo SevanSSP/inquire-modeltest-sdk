d3ea36dcc491742801553825a533260633bbaa0a
EBGlom <ebg@sevassp.com>
2023-05-12 13:20:26 +0200

    allowed parameterised api_url in functional test

--- tests/functional/test_user.py
+++ tests/functional/test_user.py
@@ -3,7 +3,8 @@ from urllib.parse import urljoin
 from modeltestSDK.config import Config
 from modeltestSDK.client import Client
 
-def test_create_user(http_service):
+
+def test_create_user(http_service, admin_key):
     """The Api is now verified good to go and tests can interact with it"""
     api_url = http_service
     user_dict = dict(username='tester494234',
@@ -11,13 +12,9 @@ def test_create_user(http_service):
                      email='test@testing.com',
                      password='password',
                      disabled=False)
-    print(api_url)
+
     config = Config
-
-
     config.host = api_url
     client = Client(config)
-    resp = requests.post(f'{api_url}/api/v1/auth/users?administrator_key=administrator', json=user_dict)
+    resp = requests.post(f'{api_url}/api/v1/auth/users?administrator_key={admin_key}', json=user_dict)
     assert resp.status_code == 200
-
-
