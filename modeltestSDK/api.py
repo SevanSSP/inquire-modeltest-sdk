de20d646ba874866fa33a38e9a6dd5594eb60ed7
EBGlom <ebg@sevassp.com>
2023-05-24 15:35:55 +0200

    Added update method to API objects. made secret key optional for deletion

--- modeltestSDK/api.py
+++ modeltestSDK/api.py
@@ -19,7 +19,7 @@ class BaseAPI:
         self._resource_path: str = format_class_name(self.__class__.__name__)
         self.client = client
 
-    def delete(self, item_id: str, secret_key: str):
+    def delete(self, item_id: str, secret_key: str = None):
         """
         Delete item by id
 
@@ -35,6 +35,23 @@ class BaseAPI:
         Deleting items requires administrator privileges.
         """
         resp = self.client.delete(self._resource_path, endpoint=item_id, parameters=dict(secret_key=secret_key))
+        return resp
+
+    def update(self, item_id: str, body: dict, secret_key: str = None):
+        """
+        Update item by id
+
+        Parameters
+        ----------
+        item_id : str
+            Item identifier
+        body : dict
+            Request body
+        secret_key : str
+            Secret key to allow update of read only items
+        """
+        resp = self.client.patch(self._resource_path, endpoint=item_id, parameters=dict(secret_key=secret_key),
+                                 body=body)
         return resp
 
 
